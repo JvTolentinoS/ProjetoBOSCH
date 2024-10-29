1f243702ca58d79e4440c5778f58b9ef8de601fd
JvTolentinoS <73199533+JvTolentinoS@users.noreply.github.com>
2024-10-29 16:42:57 -0300

    Add files via upload

--- Projeto Bosch/src/main/java/Main.java
+++ Projeto Bosch/src/main/java/Main.java
@@ -8,21 +8,23 @@ import entidade.Cliente;
 public class Main {
     public static void main(String[] args) {
 
+
+
     }
 }
 
-//          for (Carro c : carroDAO.getCarros()) {
+//  for (Carro c : carroDAO.getCarros()) {
 //            System.out.println("Carro: " +c.getPlaca());
 //        }
-
+//
 //  for (Agendamento a : agendamentoDAO.getAgendaTodos()) {
 //        System.out.println("Agenda: " + a.getDataCriacao() + " " + a.getDataAgendada() + " " + a.getCpf());
 //        }
-
-/*  for (Pedido p : pedidoDAO.getPedidosUsuario(cliente.getId_cliente())) {
-            System.out.println("Pedido: " + p.getPlaca() + " " + p.getDataCriacao());
-        } */
-
-/* for (Cliente c : clienteDAO.getClientes()) {
-            System.out.println("Cliente: " + c.getNome_cliente() + " " + c.getCpf_cliente());
-        } */
+//
+//  for (Pedido p : pedidoDAO.getPedidosUsuario(cliente.getId_cliente())) {
+//            System.out.println("Pedido: " + p.getPlaca() + " " + p.getDataCriacao());
+//        }
+//
+//  for (Cliente c : clienteDAO.getClientes()) {
+//            System.out.println("Cliente: " + c.getNome_cliente() + " " + c.getCpf_cliente());
+//        }
